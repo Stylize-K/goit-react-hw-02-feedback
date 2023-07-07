8162b35defff5b93a4219c8df2e2ab280404e469
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-07-07 21:17:01 +0300

    Add main logic to App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,65 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  addGood = () => {
+    this.setState(prevState => ({
+      good: prevState.good + 1,
+    }));
+  };
+
+  addNeutral = () => {
+    this.setState(prevState => ({
+      neutral: prevState.neutral + 1,
+    }));
+  };
+
+  addBad = () => {
+    this.setState(prevState => ({
+      bad: prevState.bad + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const total = this.countTotalFeedback();
+    const positivePercentage = Math.round((good / total) * 100);
+    return total ? positivePercentage : 0;
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
+    return (
+      <div style={{ margin: '20px' }}>
+        <h1>Please leave feedback</h1>
+        <button type="button" onClick={this.addGood}>
+          Good
+        </button>
+        <button type="button" onClick={this.addNeutral}>
+          Neutral
+        </button>
+        <button type="button" onClick={this.addBad}>
+          Bad
+        </button>
+        <h2>Statistics</h2>
+        <p>Good: {good} </p>
+        <p>Neutral: {neutral} </p>
+        <p>Bad: {bad} </p>
+        <p>Total: {total} </p>
+        <p>Positive feedback: {positivePercentage}%</p>
+      </div>
+    );
+  }
+}
