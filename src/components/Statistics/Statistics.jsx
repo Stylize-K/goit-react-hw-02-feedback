b4b128cdf86488b8d2b982635a3db61c5b9302cc
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-07-08 03:31:35 +0300

    Add some styles

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -9,12 +9,32 @@ export const Statistics = ({
   positivePercentage,
 }) => {
   return (
-    <div className={css.statWrapper}>
-      <p className={css.stat}>Good: {good} </p>
-      <p className={css.stat}>Neutral: {neutral} </p>
-      <p className={css.stat}>Bad: {bad} </p>
-      <p className={css.stat}>Total: {total} </p>
-      <p className={css.stat}>Positive feedback: {positivePercentage}%</p>
+    <div className={css.wrapper}>
+      <div className={css.statWrapper}>
+        <p className={css.stat}>
+          Good: <span className={css.value}>{good}</span>
+        </p>
+        <p className={css.stat}>
+          Neutral: <span className={css.value}>{neutral}</span>
+        </p>
+        <p className={css.stat}>
+          Bad: <span className={css.value}>{bad}</span>{' '}
+        </p>
+        <p className={css.stat}>
+          Total: <span className={css.value}>{total}</span>{' '}
+        </p>
+      </div>
+      <div className={css.posiWrapper}>
+        <p className={css.stat}>
+          Positive feedback:{' '}
+          <span className={css.value}>{positivePercentage}%</span>
+        </p>
+        <progress
+          className={css.scale}
+          max="100"
+          value={positivePercentage}
+        ></progress>
+      </div>
     </div>
   );
 };
